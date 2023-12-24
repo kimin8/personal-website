1ff3ce202a4d547060d6b28ba7e938b124dcd700
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2023-12-24 23:59:48 +0200

    Updated navigation to make it responsive

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,27 +1,51 @@
 // Navigation scrolling effect
 
 window.addEventListener('scroll', checkNavColor);
+let navToggle = document.querySelector('.mobile-nav-toggle');
+let buttons = document.querySelectorAll('#buttons > a')
+let isScrolled = navToggle.getAttribute('data-scrolled');
 
 function checkNavColor() {
+    let width = window.innerWidth;
     let header = document.getElementById('header');
     let logo = document.getElementById('logo');
-    let buttons = document.querySelectorAll('#buttons > a')
+
     if (window.scrollY > 0) {
+        
         header.classList.add('scrolled');
         logo.classList.add('scrolled-colors');
+        buttons.forEach(button => {
+            button.style.color = 'black';
+        });
+        navToggle.setAttribute('data-scrolled', "true");
+        navToggle.style.backgroundImage = "url('images/icon-hamburger-black.svg')";
 
+    } else {
+        header.classList.remove('scrolled');
+        logo.classList.remove('scrolled-colors');
+        if(width > 1170) {
+            buttons.forEach(button => {
+                button.style.color = 'white';
+            });
+        }
+
+        navToggle.setAttribute('data-scrolled', "false");
+        navToggle.style.backgroundImage = "url('images/icon-hamburger-white.svg')";
+    }
+}
+
+window.addEventListener('resize', () => {
+    let width = window.innerWidth;
+    if(width <= 1170) {
         buttons.forEach(button => {
             button.style.color = 'black';
         });
     } else {
-        header.classList.remove('scrolled');
-        logo.classList.remove('scrolled-colors');
-        
         buttons.forEach(button => {
-            button.style.color = '#f9f9f9';
+            button.style.color = 'white';
         });
     }
-}
+})
 
 // Buttons front page links
 
@@ -35,3 +59,23 @@ linkedinButton.onclick = () => {
 githubButton.onclick = () => {
     location.href = 'https://github.com/kimin8';
 }
+
+// Responsive navigaiton
+
+let primaryNav = document.querySelector('.navbar');
+
+navToggle.addEventListener('click', () => {
+
+    let visibility = primaryNav.getAttribute('data-visible');
+    
+    if(visibility === "false") {
+        primaryNav.setAttribute('data-visible', "true");
+        navToggle.setAttribute('aria-expanded', "true");
+        navToggle.style.backgroundImage = "url('images/icon-close.svg')";
+    } else {
+        primaryNav.setAttribute('data-visible', "false");
+        navToggle.setAttribute('aria-expanded', "false");
+        navToggle.style.backgroundImage = "url('images/icon-hamburger-white.svg')";
+    }
+
+});
